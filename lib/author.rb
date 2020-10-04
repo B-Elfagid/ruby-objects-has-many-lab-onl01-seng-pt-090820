fe5c46ac86319fec787dca1cfb5eead5907c4cc9
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-10-04 23:06:27 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author 
   
  attr_accessor :name, :posts 
-  @@post_count = 0
+ 
   
   def initialize(name)
     @name = name 
@@ -12,8 +12,6 @@ class Author
   end 
   
   def add_post(post)
-    @@post_count += 1
-    
     post.author = self 
   end 
   
